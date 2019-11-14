9190a63fc7c3019f46ded71dfeaaa5daba977ba7
Tyler Leigh <tylerleigh94@gmail.com>
2019-11-14 18:19:42 -0500

    Completed merge

--- Clean and Merge Waves 1-4.R
+++ Clean and Merge Waves 1-4.R
@@ -1,5 +1,10 @@
 ####Libraries####
 library(easypackages)
-libs<-c("tidyr", "car", "haven")
+libs<-c("tidyr", "dplyr", "car", "haven")
 libraries(libs)
-####Merge Data sets together####
+####Read in Data####
+dat.123 <- read_sav("Data/mergedpwavesfilesONLYall123.sav")
+dat.4 <- read_sav("Data/wave 4 cleaned.sav")
+####Merge Datasets together####
+dat.1234all<-full_join(dat.123, dat.4, by="CaseId")
+saveRDS(dat.1234all, file="waves1234all.rds")
